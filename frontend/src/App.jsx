9fef69c7af292baf29f7cf359e255a31a24a08eb
Riccardo <rbarchi09@gmail.com>
2026-09-19 11:47:39 +0200

    chore: base setup with Vite, Tailwind and initial src architecture

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-slate-900 text-white">
+      <h1 className="text-3xl font-bold underline text-blue-400">
+        Gestionale Inventario Pronto!
+      </h1>
+    </div>
+  )
+}
+
+export default App
